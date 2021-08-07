50888c4ae622551dbae2303179a4e7df3a6b128f
pungpeee <pungpeekiku@gmail.com>
2021-08-07 14:11:54 +0700

    Edit titlw with notify

--- subject-reservation-web-demo/src/views/Home.vue
+++ subject-reservation-web-demo/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <br />
     <p style="font-family: Roboto; font-size: 18pt">
-      SIT Subject Reservation Web by Pungpeee
+      SIT Subject Reservation Web by Pungpeee on netify
     </p>
     <br />
     <v-row justify="center" align="top" no-gutters style="height: 150px">
